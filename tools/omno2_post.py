535737a00272ac957f300ee1f77f16a9948e303d
Christoph Keller <christoph.a.keller@nasa.gov>
2022-03-02 10:14:14 -0500

    now filter for valid OMI NO2 observations in post-processing script

--- tools/omno2_post.py
+++ tools/omno2_post.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/local/other/python/GEOSpyD/2019.03_py3.7/2019-04-22/bin/python
 import xarray as xr
 import numpy as np
 import datetime as dt
@@ -12,11 +12,12 @@ import os
 
 def main(args):
     '''
-    Create gridded map indicating observation hour (closest integer hour) for each (NO2) analysis increment. 
+    Postprocessing script to create netCDF file with background, analysis, and increment fields plus closest 
+    (full) observation hour for each grid cell where the increment is non-zero. 
     '''
     log = logging.getLogger(__name__)
     # parse date
-    anadate = dt.datetime.strptime(args.date,'%Y-%m-%d %H:%M')
+    anadate = dt.datetime.strptime(args.date,'%Y%m%d_%H%Mz')
     # read analysis & background file 
     ana = _read_file(args.anafile,anadate)
     bkg = _read_file(args.bkgfile,anadate)
@@ -25,13 +26,11 @@ def main(args):
     tno2_hour = np.zeros((1,len(ana.lat),len(ana.lon)))
     cnt = np.zeros((24,tno2_hour.shape[1],tno2_hour.shape[2]))
     # read the preprocessed OMNO2 file
-    satfile = anadate.strftime(args.satfile)
-    tno2_hour,cnt = _process_omi_file(args,satfile,ana,tno2_hour,cnt)
+    tno2_hour,cnt = _process_omi_file(args,anadate,ana,tno2_hour,cnt)
     # calculate average values
     cntsum = np.sum(cnt,axis=0)
     obshour = cnt.argmax(axis=0)
     msk = cntsum>0.0
-    #tno2_hour[0,msk] = tno2_hour[0,msk] / cntsum[msk]
     tno2_hour[0,msk] = obshour[msk]
     # set missing values to nan
     msk = (cntsum<=0.0)
@@ -40,20 +39,25 @@ def main(args):
     ana_hour = _get_ana_hour(args,ana,bkg,tno2_hour)
 #---Create new data set and write to netCDF file
     do = xr.Dataset(
-       data_vars=dict(ana_hour=(["time","lat","lon"],ana_hour,dict(long_name='analysis_nearest_hour',units='hour'))),
+       data_vars=dict(
+          ana_no2=(["time","lev","lat","lon"],ana['NO2'],dict(long_name='analysis_no2',units='v/v')),
+          bkg_no2=(["time","lev","lat","lon"],bkg['NO2'],dict(long_name='background_no2',units='v/v')),
+          inc_no2=(["time","lev","lat","lon"],ana['NO2']-bkg['NO2'],dict(long_name='no2_analysis_increment',units='v/v')),
+          ana_hour=(["time","lat","lon"],ana_hour,dict(long_name='analysis_nearest_hour',units='hour')),
+       ),
        coords=dict(
-           {"time":("time",[anadate.hour+anadate.minute/60.],{"units":anadate.strftime("hours since %Y-%m-%d")})},
+           {"time":("time",[anadate.hour+anadate.minute/60.],{"units":anadate.strftime("hours since %Y-%m-%d 00:00:00")})},
+           lev=ana.lev,
            lon=ana.lon,
            lat=ana.lat,
        ),
        attrs=dict(description="Nearest observation hour for analysis"), 
     )
-    do.attrs['History'] = dt.datetime.now().strftime('Created by anahour.py on %Y-%m-%d %H:%M')
-    do.attrs['Author'] = 'anahour.py (written by Christoph Keller)' 
-    #do['time'].values = [anadate]
+    do.attrs['History'] = dt.datetime.now().strftime('Created by omno2_post.py on %Y-%m-%d %H:%M')
+    do.attrs['Author'] = 'omno2_post.py (written by Christoph Keller)' 
     ofile = anadate.strftime(args.outfile)
     do.to_netcdf(ofile,unlimited_dims='time')
-    log.info('Observation hour written to {}'.format(ofile))
+    log.info('Postprocessed NO2 data written to {}'.format(ofile))
 #---Cleanup
     ana.close()
     bkg.close()
@@ -63,18 +67,19 @@ def main(args):
 
 def _get_ana_hour(args,ana,bkg,tno2_hour):
     '''
-    Create map with analysis hour for each non-zero analysis increment.
+    Get the analysis hour for each column where at least one model level a non-zero analysis increment.
+    The analysis hour is defined as the observation hour of the closest available OMI observation. 
     '''
     log = logging.getLogger(__name__)
     ana_hour = np.zeros(tno2_hour.shape)*np.nan
-    # count non-zero increments per column
-    inc = ana[args.var].values[0,:,:,:] - bkg[args.var].values[0,:,:,:]
+    # get all lat/lons with at least one non-zero increment in the column
+    inc = ana['NO2'].values[0,:,:,:] - bkg['NO2'].values[0,:,:,:]
     non0 = inc!=0.0
     non0d2 = non0.sum(axis=0)
     if not np.any(non0d2>0.0):
         log.warning('No increments found - ana_hour is empty!')
         return ana_hour
-    # get vector of all lat/lons with non-nan observations for more convenient indexing below
+    # get vector of all lat/lons with valid entries for more convenient indexing below
     gridlats = ana.lat.values[:].repeat(len(ana.lon))
     gridlons = np.tile(ana.lon.values,len(ana.lat))
     obshour = tno2_hour.flatten()
@@ -82,8 +87,6 @@ def _get_ana_hour(args,ana,bkg,tno2_hour):
     obshour = obshour[obsmsk]
     obslats = gridlats[obsmsk]
     obslons = gridlons[obsmsk]
-    #obslonidx = [np.abs(ana.lon.values-i).argmin() for i in obslons] 
-    #obslatidx = [np.abs(ana.lat.values-i).argmin() for i in obslats]
     # go over all lat/lons with an increment and assign the obs hour to it
     non01d = non0d2.flatten()
     anamsk = non01d>0.0
@@ -96,51 +99,25 @@ def _get_ana_hour(args,ana,bkg,tno2_hour):
     idxs = [np.array((obslons-i)**2+(obslats-j)**2).argmin() for i,j in zip(analons,analats)]
     ana_hour[0,analatidx,analonidx] = obshour[idxs]
     return ana_hour
-#    ihour = tno2_hour[0,analatidx,analonidx]
-#    ana_hour[0,analatidx,analonidx] = ihour
-#    # check for nans 
-#    imsk = np.isnan(ihour)
-#    if np.any(imsk):
-#        nanlats = np.array(analats)[imsk]
-#        nanlons = np.array(analons)[imsk]
-#        nanlatidxs = np.array(analatidx)[imsk]
-#        nanlonidxs = np.array(analonidx)[imsk]
-#        idxs = [np.array((obslons-i)**2+(obslats-j)**2).argmin() for i,j in zip(nanlons,nanlats)]
-#        ana_hour[0,nanlatidxs,nanlonidxs] = obshour[idxs]
-#        for i in range(len(nanlats)):
-#            #nanlat = nanlats[i]
-#            #nanlon = nanlons[i]
-#            ilatidx = nanlatidxs[i]
-#            ilonidx = nanlonidxs[i]
-#            #distance_to_cell = [(nanlon-i)**2+(nanlat-j)**2 for i,j in zip(obslons,obslats)]
-#            distance_to_cell = [(ilonidx-i)**2+(ilatidx-j)**2 for i,j in zip(obslonidx,obslatidx)]
-#            idx = np.array(distance_to_cell).argmin()
-#            ana_hour[0,ilatidx,ilonidx] = obshour[idx]
-# old loop:
-#    for iana in range(len(non01d)):
-#        analon = analons[i]
-#        analat = analats[i]
-#        ihour = tno2_hour[0,analat,analon]
-#        # if nan, get closest non-nan observation
-#        if np.isnan(ihour):
-#            distance_to_cell = [(analon-i)**2+(analat-j)**2 for i,j in zip(obslons,obslats)]
-#            idx = distance_to_cell.argmin()
-#            ihour = obshour[idx]
-#        ana_hour[0,analat,analon] = ihour
 
  
-def _process_omi_file(args,satfile,ana,tno2_hour,cnt):
+def _process_omi_file(args,anadate,ana,tno2_hour,cnt):
     '''
     Read single OMI NO2 file and put nearest hour of (non-nan) observations onto a grid.
     '''
     log = logging.getLogger(__name__)
-    if not os.path.isfile(satfile):
-        log.error('Satellite file not found: {}'.format(satfile))
-        return
-    log.info('Reading {}'.format(satfile))
-    ds = xr.open_dataset(satfile)
+    ds = _read_file(args.satfile,anadate) 
     tno2_all = ds.variables['ColumnAmountNO2Trop'].values[:]
-    mask = ~np.isnan(tno2_all)
+    albd_all = ds.variables['TerrainReflectivity'].values[:]
+    cldf_all = ds.variables['CloudRadianceFraction'].values[:]
+    sza_all  = ds.variables['SolarZenithAngle'].values[:]
+    scd_all  = ds.variables['SlantColumnAmountNO2Destriped'].values[:]
+    flag_all = ds.variables['VcdQualityFlags'].values[:].astype(int)&1   # bitwise and operation
+    albd_all[np.isnan(albd_all)] = 0.0
+    cldf_all[np.isnan(cldf_all)] = 0.0
+    scd_all[np.isnan(scd_all)]   = -999.0
+    #mask = ~np.isnan(tno2_all)
+    mask = ( (flag_all==0) & (albd_all<=300.0) & (cldf_all<=500.0) & (scd_all>0.0) & (sza_all<=80.0) )
     tno2 = tno2_all[mask].copy()
     lats = ds.variables['Latitude'].values[mask].copy()
     lons = ds.variables['Longitude'].values[mask].copy()
@@ -179,7 +156,7 @@ def _process_omi_file(args,satfile,ana,tno2_hour,cnt):
 
 
 def _read_file(template,idate):
-    '''Read file from file template for given data'''
+    '''Read file from template for given date'''
     log = logging.getLogger(__name__)
     ifile = idate.strftime(template)
     if not os.path.isfile(ifile):
@@ -191,11 +168,10 @@ def _read_file(template,idate):
 
 def parse_args():
     p = argparse.ArgumentParser(description='Undef certain variables')
-    p.add_argument('-d', '--date',type=str,help='date in format %Y-%m-%d %H:%M',default='2018-07-01 12:00')
+    p.add_argument('-d', '--date',type=str,help='date in format %Y%m%d %H%Mz',default='20180701 1200z')
     p.add_argument('-a', '--anafile',type=str,help='analysis file',default='/discover/nobackup/cakelle2/CDAS/runs/omno2_test/scratch/omno2_test.ana.eta.%Y%m%d_%Hz.nc4')
     p.add_argument('-b', '--bkgfile',type=str,help='background file',default='/discover/nobackup/cakelle2/CDAS/runs/omno2_test/temp/omno2_test.cbkg.eta.%Y%m%d_%Hz.nc4')
     p.add_argument('-s', '--satfile',type=str,help='satellite file',default='/discover/nobackup/cakelle2/CDAS/runs/omno2_test/analyze/spool/omno2.%Y%m%d.t%Hz.nc')
-    p.add_argument('-v', '--var',type=str,help='file variable of interest',default='NO2')
     p.add_argument('-o', '--outfile',type=str,help='output file',default='ana_hours.%Y%m%d_t%Hz.nc')
     return p.parse_args()    
 
@@ -207,5 +183,4 @@ if __name__ == '__main__':
     handler.setLevel(logging.INFO)
     log.addHandler(handler)
     main(parse_args())
-
 
